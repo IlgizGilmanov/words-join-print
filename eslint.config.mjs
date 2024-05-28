c89d50039ecb3d5a6b45f32047a82a6412365cf5
Ilgiz Gilmanov <ilgiz.gilmanov@flatstack.com>
2024-05-29 00:56:21 +0200

    Setup next.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,13 +4,24 @@ import tseslint from 'typescript-eslint';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 import { fixupConfigRules } from '@eslint/compat';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
 
 export default [
   {
     languageOptions: {
-      globals: globals.browser,
+      ...globals.browser,
+      ...nextCoreWebVitals.languageOptions?.globals, // Add globals from next/core-web-vitals
     },
-    extends: ['prettier'],
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the React version
+      },
+      ...nextCoreWebVitals.settings, // Add settings from next/core-web-vitals
+    },
+    rules: {
+      ...eslintConfigPrettier.rules, // Rules from Prettier
+      ...nextCoreWebVitals.rules, // Rules from next/core-web-vitals
+    },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
